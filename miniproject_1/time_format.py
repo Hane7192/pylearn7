6055b84b4b21c6ca3fed3f2023d68537512f1d4d
Hane7192 <hane.naghshbandi95@gmail.com>
2023-01-10 01:08:54 +0330

    first commit

--- miniproject_1/time_format.py
+++
@@ -1,8 +0,0 @@
-class My_Time:
-    def __init__(self, hour, minute):
-        self.h = hour
-        self.m = minute
-
-    def time_to_min(self):
-        result = self.h*60 + self.m
-        return result
